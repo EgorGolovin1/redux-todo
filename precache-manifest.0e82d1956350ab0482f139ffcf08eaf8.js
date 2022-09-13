519171205e6a37c66c9e24dd552b7db643af7385
Egor Golovin <egorwork99@mail.ru>
2022-09-13 21:43:23 +0300

    Updates

---
+++ precache-manifest.0e82d1956350ab0482f139ffcf08eaf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81bb141f25b0116b2b2a",
+    "url": "/redux-todo/static/css/main.6f1bd52e.chunk.css"
+  },
+  {
+    "revision": "81bb141f25b0116b2b2a",
+    "url": "/redux-todo/static/js/main.81bb141f.chunk.js"
+  },
+  {
+    "revision": "2110d2859e344eff7468",
+    "url": "/redux-todo/static/js/1.2110d285.chunk.js"
+  },
+  {
+    "revision": "e97dda427c85cde9b7b5",
+    "url": "/redux-todo/static/js/runtime~main.e97dda42.js"
+  },
+  {
+    "revision": "05f863898c5ee9a1ebf8e77716ba0135",
+    "url": "/redux-todo/static/media/phill.05f86389.png"
+  },
+  {
+    "revision": "d69e1dbf2d76e5fe564a78d0516be66f",
+    "url": "/redux-todo/index.html"
+  }
+];
